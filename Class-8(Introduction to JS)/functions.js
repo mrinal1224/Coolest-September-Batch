dc94597bdbdf5e16faf134d1a63c9e2d311e920e
mrinal1224 <iammrinalkc@gmail.com>
2024-10-14 23:23:26 +0530

    function

---
+++ Class-8(Introduction to JS)/functions.js
@@ -0,0 +1,14 @@
+function greet(){
+    console.log('Hello')
+}
+
+greet() // fucnction invokation , function call
+
+// Function to server beverages
+
+function serveDrink(drink){
+  console.log(drink + ' served')
+}
+
+serveDrink('tea')
+
