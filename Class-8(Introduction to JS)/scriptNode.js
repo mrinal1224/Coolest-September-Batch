5f0f2dfb8e1704b89acbe24c2d984914adcc28ed
mrinal1224 <iammrinalkc@gmail.com>
2024-10-14 23:00:11 +0530

    push

---
+++ Class-8(Introduction to JS)/scriptNode.js
@@ -0,0 +1,28 @@
+//  How to comment in JS
+//  Variables in JS
+
+let a = 20
+console.log(a) // Number
+
+
+a = 'Hello'
+console.log(a)
+
+const c = 3.14
+
+ c  = 9.8
+
+console.log(c)
+
+
+
+
+// var b = "S" // String
+// console.log(typeof b)
+
+// var c = false // boolean
+// console.log(typeof c)
+
+
+
+
