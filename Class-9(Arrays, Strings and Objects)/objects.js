ce4be8c4ef415344b888a6835bac8264ebd7afdd
mrinal1224 <iammrinalkc@gmail.com>
2024-10-16 23:33:48 +0530

    objects

--- Class-9(Arrays, Strings and Objects)/objects.js
+++ Class-9(Arrays, Strings and Objects)/objects.js
@@ -1,43 +1,67 @@
 let person1 = {
-    name : 'Adam',
-    age: 24,
-    ownsAcar: true,
-    gender: 'M'
-}
+  name: "Adam",
+  age: 24,
+  ownsAcar: true,
+  gender: "M",
+};
 
-let person2={
-    name : 'Steve',
-    age: 20,
-    ownsAcar: false,
-    gender: 'M'
-}
+let person2 = {
+  name: "Steve",
+  age: 20,
+  ownsAcar: false,
+  gender: "M",
+};
 
-console.log(person1)
-console.log(person2)
-
+console.log(person1);
+console.log(person2);
 
 // let's create an obj for captain america
 
-let cap={
-    firstName: 'Steve',
-    lastName : 'Rogers',
-    age : 110,
+let cap = {
+  firstName: "Steve",
+  lastName: "Rogers",
+  age: 110,
 
-    allies:['Tony stark' , 'Hulk' , 'Thor'],
+  allies: ["Tony stark", "Hulk", "Thor"],
 
-    isAvenger : true,
+  isAvenger: true,
 
-    address:{
-        country:'USA',
-        city:{
-            cityName : 'Brooklyn',
-            pincode : 123456
-        }
+  address: {
+    country: "USA",
+    city: {
+      cityName: "Brooklyn",
+      pincode: 123456,
     },
+  },
 
-    sayHi : function(){
-        console.log('Cap says Hi')
-    }
+  sayHi: function() {
+    console.log("Cap says Hi");
+  },
+};
 
 
-}
+// dot notation bracket notation
+
+console.log(cap.firstName) // dot notation
+console.log(cap['age'])
+
+
+console.log(cap.address.city.cityName)
+
+cap.allies.push('Bucky')
+
+console.log(cap.allies[1])
+
+cap.isAvenger = false
+
+cap.movies= ['Age of Ultron' , 'Winter Solider']
+
+
+
+delete cap.age
+
+console.log(cap)
+
+cap.sayHi()
+
+
