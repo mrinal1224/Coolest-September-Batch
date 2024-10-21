504131e00de5b45377735710e2369dc729c7c215
mrinal1224 <iammrinalkc@gmail.com>
2024-10-21 23:00:19 +0530

    push

---
+++ Class-10(Execution Context and Hositing)/hoisting.js
@@ -0,0 +1,45 @@
+// console.log(a) // undefined
+// sayHi() // this always works
+
+// var a = 20
+
+// var b = 30
+
+
+// function sayHi(){
+//     console.log('Hi')
+// }
+
+
+// var message = greet()
+// console.log(message)
+
+// // function as a variable or function as expressions or functions as first class citizen
+// var greet = function(){
+//     return `Hello`
+// }
+
+
+
+
+let d = 30
+
+
+function add(){
+    console.log(2+d)
+}
+
+add()
+
+
+
+
+
+
+
+
+
+
+
+
+
