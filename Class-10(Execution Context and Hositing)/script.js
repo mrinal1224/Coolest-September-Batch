139304559106ccb0f935294a2976a0f6d1b68549
mrinal1224 <iammrinalkc@gmail.com>
2024-10-21 21:16:13 +0530

    push

---
+++ Class-10(Execution Context and Hositing)/script.js
@@ -0,0 +1,12 @@
+var a=20
+var b=30
+
+function add(num1 ,num2){
+   var ans=num1+num2
+   return ans
+}
+
+var sum1 = add(a ,b)
+var sum2 = add(3,4)
+console.log(sum1) // 50
+console.log(sum2) // 7
