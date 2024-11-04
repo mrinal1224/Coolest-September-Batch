8cee01c579d8337d5c448491bca9a58b82c64b10
mrinal1224 <iammrinalkc@gmail.com>
2024-11-04 21:32:50 +0530

    push

---
+++ Class-13 (Async Programming-1)/script.js
@@ -0,0 +1,19 @@
+// console.log('Start')
+
+// function test(){
+//     console.log('Hello')
+// }
+// test()
+
+// console.log('End')
+
+console.log('Start')
+
+function sayHello(){
+    console.log("Hello")
+} // 2-3 seconds
+
+setTimeout(sayHello , 0)
+
+console.log('End')
+
