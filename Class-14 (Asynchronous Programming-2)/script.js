f9f5328dab77f9c5c91ca00182918b5c79e3393b
mrinal1224 <iammrinalkc@gmail.com>
2024-11-06 21:53:19 +0530

    push

--- Class-14 (Asynchronous Programming-2)/script.js
+++ Class-14 (Asynchronous Programming-2)/script.js
@@ -1,6 +1,7 @@
-let promise = new Promise(function(resolve , reject){
+let promise1 = new Promise(function(resolve , reject){
     setTimeout(function(){
         const isHeads = Math.random() >= 0.5 // Heads
+        console.log(isHeads)
         if(isHeads){
             resolve('We got Heads Promise Fulfilled')
         }
@@ -8,7 +9,31 @@ let promise = new Promise(function(resolve , reject){
         else{
             reject ('We got tails and the Promise Failed')
         }
-    })
+    } , 1000)
 })
 
-console.log(promise)
+// Handling promises
+//then - resolve
+//catch - reject
+//finally - settled
+
+
+// Success
+promise1.then(function(output){
+    console.log(output)
+})
+
+// Failure
+promise1.catch(function(error){
+    console.log(error)
+})
+
+// Settled
+
+promise1.finally(function(){
+    console.log('Promise settled')
+})
+
+
+
+
