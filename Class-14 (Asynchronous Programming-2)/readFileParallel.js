f7944a44b84efb8e2d44cdb032e9bf24e5ce4245
mrinal1224 <iammrinalkc@gmail.com>
2024-11-06 22:47:18 +0530

    promise parallel

---
+++ Class-14 (Asynchronous Programming-2)/readFileParallel.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+
+let promiseReadFile1 = fs.promises.readFile('f1.txt')
+let promiseReadFile2 = fs.promises.readFile('f2.txt')
+let promiseReadFile3 = fs.promises.readFile('f3.txt')
+
+// For File 1
+promiseReadFile1.then(function(data) {
+    console.log('This is file 1 data -> ' + data)
+}).catch(function(err) {
+    console.log('This is Your Error -> ' + err)
+})
+
+// For File 2
+promiseReadFile2.then(function(data) {
+    console.log('This is file 2 data -> ' + data)
+}).catch(function(err) {
+    console.log('This is Your Error -> ' + err)
+})
+
+// For File 3
+promiseReadFile3.then(function(data) {
+    console.log('This is file 3 data -> ' + data)
+}).catch(function(err) {
+    console.log('This is Your Error -> ' + err)
+})
