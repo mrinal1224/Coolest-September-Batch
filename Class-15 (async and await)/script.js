b0367bd245b7eedae24bfb17bde5252ce1dc4d86
mrinal1224 <iammrinalkc@gmail.com>
2024-11-08 22:02:52 +0530

    async await

---
+++ Class-15 (async and await)/script.js
@@ -0,0 +1,59 @@
+const p1 = new Promise(function(resolve , reject){
+    setTimeout(function(){
+        resolve('The Promise has been Resolved')
+    } , 5000)
+   
+})
+
+const p2 = new Promise(function(resolve , reject){
+    setTimeout(function(){
+        resolve('The Promise has been Resolved')
+    } , 10000)
+   
+})
+
+
+
+function resolvePromise(){
+    p1.then(function(result){
+        console.log(result)
+    })
+
+    console.log('Scaler')
+}
+
+async function handlePromise(){
+    console.log('Hello')
+    let val1 =  await p1
+    console.log("Scaler")
+    console.log(val1)
+
+    let val2 =  await p2
+    console.log("Scaler 2")
+    console.log(val2)
+}
+
+//  resolvePromise()
+
+  handlePromise()
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  async function printData(){
+//    return "hello"
+// }
+
+// let dataValue = printData()
+
+// console.log(dataValue)
+
