f9f5212e577e171c6f28e6b4eca78b1624644b0b
mrinal1224 <iammrinalkc@gmail.com>
2024-11-15 23:38:35 +0530

    push

--- Class-18(Create a Weather App)/weatherApp/script.js
+++ Class-18(Create a Weather App)/weatherApp/script.js
@@ -1,32 +1,47 @@
+const form = document.querySelector("form");
+const searchField = document.querySelector(".searchField");
+const currentLocation = document.querySelector('.time_location p')
+const dateTime = document.querySelector('.time_location span')
+const temprature = document.querySelector('.temp')
+const conditionField = document.querySelector('.weather_condition span')
 
 
-const form = document.querySelector('form')
-const searchField = document.querySelector('.searchField')
+form.addEventListener("submit", search);
 
-form.addEventListener('submit' , search)
-
-function search(e){
-    e.preventDefault()
-   fetchData(searchField.value)
+function search(e) {
+  e.preventDefault();
+  fetchData(searchField.value);
 }
 
-async function fetchData(target){
-    try {
-        const url  =`http://api.weatherapi.com/v1/current.json?key=35af7ff606db422880d141328231305&q=${target}&aqi=yes`
+async function fetchData(target) {
+  try {
+    const url = `http://api.weatherapi.com/v1/current.json?key=35af7ff606db422880d141328231305&q=${target}&aqi=yes`;
 
-        // fetch
-        const respone = await fetch(url)
+    // fetch
+    const respone = await fetch(url);
 
-        const data = await respone.json()
+    const data = await respone.json();
 
-        
+     let currentLocation = data.location.name
+     let currentCondition = data.current.condition.text
+     let currentTemp = data.current.temp_c
+     let currentTime = data.location.localtime
 
-        console.log(data)
-        
-        
-    } catch (error) {
-        console.error('Cannot Fetch')
-    }
+     updateValues(currentTime , currentLocation ,currentCondition , currentTemp )
+
+    
+    console.log(data);
+  } catch (error) {
+    console.error("Cannot Fetch");
+  }
 }
 
-fetchData('Mumbai')
+function updateValues(time , location , condition , temp){
+     dateTime.innerText = time
+     currentLocation.innerText = location
+     conditionField.innerText = condition
+     temprature.innerText = temp
+
+}
+
+fetchData("Mumbai");
