b82f770463198286f967a09caab6fd4af57d5cbf
mrinal1224 <iammrinalkc@gmail.com>
2024-11-15 22:22:35 +0530

    push

---
+++ Class-18(Create a Weather App)/script.js
@@ -0,0 +1,15 @@
+const toys = {
+    toy1: {name :'Teddy Bear' , color : 'Brown'},
+    toy2: {name :'Race Car' , color : 'Red'},
+    toy3: {name :'Doll' , color : 'Pink'}
+}
+
+const jsonData = JSON.stringify(toys)
+
+// JSON.parse
+
+const objVal = JSON.parse(jsonData)
+
+console.log(toys)
+console.log(jsonData)
+console.log(objVal)
