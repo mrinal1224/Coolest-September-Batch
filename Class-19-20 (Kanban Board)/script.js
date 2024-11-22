b28fe4e61155ab3bf922be901ab82d354ea590e0
mrinal1224 <iammrinalkc@gmail.com>
2024-11-22 22:58:13 +0530

    push

--- Class-19-20 (Kanban Board)/script.js
+++ Class-19-20 (Kanban Board)/script.js
@@ -2,6 +2,12 @@ const addBtn = document.querySelector(".add-btn");
 const modalCont = document.querySelector(".modal-cont");
 const textArea = document.querySelector(".text-area");
 const mainCont = document.querySelector('.main-cont')
+const allPriorityColors = document.querySelectorAll('.priority-color')
+let modalPriorityColor ='lightpink'
+
+
+
+
 
 let addBtnFlag = false;
 
@@ -17,10 +23,10 @@ addBtn.addEventListener("click", function () {
 
 // generating a Ticket
 
-function createTicket(task , id) {
+function createTicket(taskColor , task , id) {
   const ticketCont = document.createElement("div");
   ticketCont.setAttribute("class", "ticket-cont");
-  ticketCont.innerHTML = `<div class="ticket-color"></div>
+  ticketCont.innerHTML = `<div class="${taskColor} ticket-color"></div>
           <div class="ticket-id">${id}</div>
           <div class="task-area">${task}</div>
           <div class="ticket-lock"></div>`;
@@ -30,14 +36,33 @@ mainCont.appendChild(ticketCont)
 
 
 
+
 // Attaching key event on the Modal
 
 modalCont.addEventListener("keydown", function (e) {
   if (e.key === "Shift") {
     const task = textArea.value;
     const id = (Math.random() * 10000).toFixed(0)
-    createTicket(task , id);
+    createTicket(modalPriorityColor, task , id);
     modalCont.style.display = "none";
     addBtnFlag = false
   }
 });
+
+allPriorityColors.forEach(function(colorElem){
+    colorElem.addEventListener('click', function(){
+       allPriorityColors.forEach(function(priortyColors){
+        priortyColors.classList.remove('active')
+       })
+
+       colorElem.classList.add('active')
+
+       modalPriorityColor = colorElem.classList[0]
+
+       console.log(modalPriorityColor)
+
+
+    })
+})
+
+
