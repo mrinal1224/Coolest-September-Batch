6eadcde5a6f6774f6008f2ecfd99c314d5435dce
mrinal1224 <iammrinalkc@gmail.com>
2024-11-29 21:51:09 +0530

    push

--- Class-19-20 (Kanban Board)/splice.js
+++ Class-19-20 (Kanban Board)/splice.js
@@ -1,5 +1,15 @@
-const arr = ['mango' , 'apple' , 'strawberry' , 'papaya']
+// const arr = ['mango' , 'apple' , 'strawberry' , 'papaya']
 
-const updatedArr = arr.splice(3 , 1)
+// const updatedArr = arr.splice(3 , 1)
 
-console.log(updatedArr)
+// console.log(updatedArr)
+
+
+let arr = [1,2,3,4,5]
+
+let sum = arr.reduce(function(acc ,curr){
+     acc = acc+curr //15
+     return acc
+} ,0 )
+
+console.log(sum)
