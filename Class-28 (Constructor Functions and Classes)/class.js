1ba2a750ad22ccc14c8a7d9b56dfc4f34be14e87
mrinal1224 <iammrinalkc@gmail.com>
2024-12-13 23:33:26 +0530

    static methods

--- Class-28 (Constructor Functions and Classes)/class.js
+++ Class-28 (Constructor Functions and Classes)/class.js
@@ -10,6 +10,19 @@ class Car{
     drive(){
         console.log(`I am driving ${this.name} at ${this.topSpeed}`)
     }
+
+   
+
+
+    static compareSpeed(car1, car2) {
+        if (car1.topSpeed > car2.topSpeed) {
+            console.log(`${car1.name} is faster than ${car2.name}`);
+        } else if (car1.topSpeed < car2.topSpeed) {
+            console.log(`${car2.name} is faster than ${car1.name}`);
+        } else {
+            console.log(`${car1.name} and ${car2.name} have the same top speed`);
+        }
+    }
 }
 
 class ElectricCar extends Car {
@@ -18,7 +31,12 @@ class ElectricCar extends Car {
         this.batteryCapcity = BatteryCapcity
         this.timeToCharge = TimeToCharge
       }
+
+      
 }
+
+
+
 
 
 
@@ -35,15 +53,17 @@ const car4 = new Car('Ferrai' , 'GT' , 'blue' , '200KM/hr')
 
 const evCar1 = new ElectricCar('Tesla' , 'Model-S' , 'White' , '350KM/hr' , '100' , '3hrs')
 
-console.log(car1)
-console.log(car2)
-console.log(car3)
-console.log(car4)
 
-console.log(evCar1)
+// console.log(car1)
+// console.log(car2)
+// console.log(car3)
+// console.log(car4)
+
+// console.log(evCar1)
+Car.compareSpeed(car1 , car2)
 
 
-car1.drive()
-car2.drive()
-car3.drive()
-car4.drive()
+// car1.drive()
+// car2.drive()
+// car3.drive()
+// car4.drive()
