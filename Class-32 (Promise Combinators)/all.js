6eb7674ab423b3921bfda3a0025cda85fd733d7a
mrinal1224 <iammrinalkc@gmail.com>
2025-01-02 21:12:22 +0530

    push

---
+++ Class-32 (Promise Combinators)/all.js
@@ -0,0 +1,11 @@
+function fetchUserData() {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve({ userId: 1, username: "JohnDoe" }), 1000);
+    });
+}
+
+function fetchUserPosts() {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve(["Post 1", "Post 2", "Post 3"]), 1000);
+    });
+}
