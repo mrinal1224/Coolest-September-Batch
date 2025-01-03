5a56c4bdd9caf099d86e48bf585ab614fff57840
mrinal1224 <iammrinalkc@gmail.com>
2025-01-03 22:26:35 +0530

    deep and shallow copy

---
+++ Class-33 (Deep and Shallow Copy)/shallow.js
@@ -0,0 +1,47 @@
+let zoo1 = {
+    name: "Amazing Zoo",
+    location: "Melbourne, Australia",
+    animals: [
+      {
+        species: "Lion",
+        favoriteTreat: "Meat",
+      },
+      {
+        species: "Panda",
+        favoriteTreat: "Leaves",
+      },
+    ],
+  };
+
+
+//   let arr = [1,2,3 ,[3 ,4 ,5 , {'name':'Atul'}]]
+
+// let zoo2 = zoo1 // wrong way of making copy
+
+// spread operator
+
+// let zoo1shallowCopy = {...zoo1} // Shallow Copy
+
+// Shallow copy only works on the topmost layer of 
+// the object it cannot go deep down tp nested properties
+
+// zoo1shallowCopy.location = 'Kerela , India'
+// zoo1shallowCopy.animals[1].species = 'Elephant'
+
+// deep copy
+
+let zooString = JSON.stringify(zoo1)
+console.log(zooString)
+let deepCopyZoo = JSON.parse(zooString)
+deepCopyZoo.location = 'Kerela , India'
+deepCopyZoo.animals[1].species = "Elephant"
+
+console.log(zoo1)
+console.log(deepCopyZoo)
+
+
+
+
+
+
+// console.log(zoo1shallowCopy)
---
+++ Class-33 (Deep and Shallow Copy)/test.js
@@ -0,0 +1,10 @@
+const person1 = {
+    name : "adam",
+    age : 23
+}
+
+person1.name = 'Steve'
+
+console.log(person1)
+
+person1 = 'Hello'
