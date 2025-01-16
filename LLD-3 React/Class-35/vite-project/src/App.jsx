27df533f2f0b3af6c16f0330de6fb164eb76f6d3
mrinal1224 <iammrinalkc@gmail.com>
2025-01-16 22:33:01 +0530

    counter component

--- LLD-3 React/Class-35/vite-project/src/App.jsx
+++ LLD-3 React/Class-35/vite-project/src/App.jsx
@@ -1,22 +1,25 @@
 import Card from "./components/Card";
+import Counter from "./components/Counter";
 import Navbar from "./components/Navbar";
 
 function App() {
-  const cardData = [
-    { title: "Laptops", description: "This is a Macbook Pro M3" },
-    { title: "Apple Iphone", description: "This is an Apple Iphone 16" },
-    { title: "Nike Shoes", description: "These are Air max Shoes." },
-    { title: "Sunglasses", description: "These are RayBan Sunglasses" },
-  ];
+  // const cardData = [
+  //   { title: "Laptops", description: "This is a Macbook Pro M3" },
+  //   { title: "Apple Iphone", description: "This is an Apple Iphone 16" },
+  //   { title: "Nike Shoes", description: "These are Air max Shoes." },
+  //   { title: "Sunglasses", description: "These are RayBan Sunglasses" },
+  // ];
 
   return (
     <div>
-      <Navbar />
+      {/* <Navbar />
       <div style={{ display: "flex", justifyContent: "space-evenly" }}>
         {cardData.map((item)=> {
          return <Card cardTitle={item.title} cardDescription={item.description} />
         })}
-      </div>
+      </div> */}
+
+      <Counter/>
     </div>
   );
 }
---
+++ LLD-3 React/Class-35/vite-project/src/components/Counter.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount(count + 1);
+  }
+
+  function decrement() {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  }
+
+  function reset() {
+    setCount(0);
+  }
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Simple Counter</h1>
+      <h2>{count}</h2>
+      <button onClick={increment} style={{ marginRight: "10px" }}>
+        + Increment
+      </button>
+      <button onClick={decrement} style={{ marginRight: "10px" }}>
+        - Decrement
+      </button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
+
+export default Counter;
