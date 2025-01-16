ace617102a96f1f26bc8026883c09c2794256161
mrinal1224 <iammrinalkc@gmail.com>
2025-01-16 21:29:11 +0530

    conditional rendering

--- LLD-3 React/Class-35/vite-project/src/components/Navbar.jsx
+++ LLD-3 React/Class-35/vite-project/src/components/Navbar.jsx
@@ -1,20 +1,34 @@
-
-import "./Navbar.css"
+import "./Navbar.css";
 
 function Navbar() {
+  let user = "Mark";
+  let isLoggedIn = true;
   return (
     <div className="navbar">
-    <div className="navbar-logo">Amazon</div>
-    <div className="navbar-search">
-      <input type="text" placeholder="Search for products" className="search-input" />
-      <button className="search-button">Search</button>
+      <div className="navbar-logo">Amazon</div>
+      <div className="navbar-search">
+        <input
+          type="text"
+          placeholder="Search for products"
+          className="search-input"
+        />
+        <button className="search-button">Search</button>
+      </div>
+      <div className="navbar-links">
+        <a href="#deals" className="nav-link">
+          Today's Deals
+        </a>
+        <a href="#cart" className="nav-link">
+          Cart
+        </a>
+        {isLoggedIn ? (
+          <span>Welcome {user}</span>
+        ) : (
+          <button className="nav-login-button">Sign in</button>
+        )}
+      </div>
     </div>
-    <div className="navbar-links">
-      <a href="#deals" className="nav-link">Today's Deals</a>
-      <a href="#cart" className="nav-link">Cart</a>
-    </div>
-  </div>
   );
 }
 
-export default Navbar
+export default Navbar;
