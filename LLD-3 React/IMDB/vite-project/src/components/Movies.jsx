7f28f14d9d52285381744d2a44dff5380731a504
mrinal1224 <iammrinalkc@gmail.com>
2025-01-27 23:08:18 +0530

    api fetching and pagination and populated

--- LLD-3 React/IMDB/vite-project/src/components/Movies.jsx
+++ LLD-3 React/IMDB/vite-project/src/components/Movies.jsx
@@ -1,17 +1,34 @@
 import React, { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
 import axios from "axios";
+import Pagination from "./Pagination";
 
 function Movies() {
   // creating state for movies data
   const [movies, setMovies] = useState([]);
+  const [page , setPage] = useState(1)
 
-    console.log(movies)
+
+
+ // pagination next
+
+ const handleNext=()=>{
+   setPage(page+1)
+ }
+
+ // pagination previous
+
+ const handlePrevious=()=>{
+    if(page>1){
+        setPage(page-1)
+    }
+
+ }
 
   useEffect(() => {
     axios
       .get(
-        "https://api.themoviedb.org/3/trending/movie/day?api_key=3aec63790d50f3b9fc2efb4c15a8cf99&language=en-US"
+        `https://api.themoviedb.org/3/trending/movie/day?api_key=3aec63790d50f3b9fc2efb4c15a8cf99&language=en-US&page=${page}`
       )
       .then((respone) => {
         setMovies(respone.data.results);
@@ -19,15 +36,20 @@ function Movies() {
       .catch((err) => {
         console.error(err + " Cannot fetch API data");
       });
-  }, []);
+  }, [page]);
 
   return (
+    <div>
     <div className="flex justify-evenly flex-wrap gap-8"  >
      {
         movies.map((movieObj)=>(
             <MovieCard movieObject={movieObj} />
         ))
      }
+
+
+    </div>
+     <Pagination pageNumber={page}  previousFn={handlePrevious} nextFn={handleNext}/>
     </div>
   );
 }
---
+++ LLD-3 React/IMDB/vite-project/src/components/Pagination.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Pagination({pageNumber , nextFn , previousFn}) {
+
+  return (
+    <div
+      className="bg-gray-400 h-[50px] w-full
+      mt-8 flex justify-center gap-4"
+    >
+      <div onClick={previousFn} className="px-8">
+        <i class="fa-solid fa-arrow-left"></i>
+      </div>
+      <div>{pageNumber}</div>
+      <div onClick={nextFn} className="px-8">
+        
+        <i class="fa-solid fa-arrow-right"></i>
+      </div>
+    </div>
+  );
+}
+
+export default Pagination;
