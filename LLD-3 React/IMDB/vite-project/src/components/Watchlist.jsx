1a5bece68ee3e2bc073960e4df12aada296b7483
mrinal1224 <iammrinalkc@gmail.com>
2025-01-31 20:15:10 +0530

    push

--- LLD-3 React/IMDB/vite-project/src/components/Watchlist.jsx
+++ LLD-3 React/IMDB/vite-project/src/components/Watchlist.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-
 function Watchlist() {
   return (
     <>
@@ -10,27 +9,33 @@ function Watchlist() {
         {/* {search Bar} */}
       </div>
 
-      <div className='m-8'>
-        <table className=''>
-          <thead className='border border-gray-200 rounded-lg bg-gray-200'>
-            <tr className="flex justify-evenly">
-              <th>Name</th>
-              <th>Ratings</th>
-              <th>Popularity</th>
-              <th>Genre</th>
-              <th>Delete Movies</th>
+      <div className="m-8 w-full overflow-x-auto">
+        <table className="w-full table-auto border-collapse">
+          <thead className="border border-gray-200 bg-gray-200">
+            <tr>
+              <th className="px-6 py-4 text-center">Name</th>
+              <th className="px-6 py-4 text-center">Ratings</th>
+              <th className="px-6 py-4 text-center">Popularity</th>
+              <th className="px-6 py-4 text-center">Genre</th>
+              <th className="px-6 py-4 text-center">Delete Movies</th>
             </tr>
           </thead>
           <tbody>
-            <tr className='border-b-2'>
-              <td className='flex items-center px-6 py-4'>
-                <img className='h-[6rem] w-[10rem]' src='https://i.pinimg.com/originals/29/7d/e0/297de0761b0c756266d74ca50d03cc1d.jpg'/>
-                <div className='mx-10'>Movie Title</div>
+            <tr className="border-b-2">
+              <td className="px-6 py-4 flex items-center space-x-4">
+                <img
+                  className="h-[6rem] w-[10rem]"
+                  src="https://i.pinimg.com/originals/29/7d/e0/297de0761b0c756266d74ca50d03cc1d.jpg"
+                  alt="Movie"
+                />
+                <div>Movie Title</div>
               </td>
-              <td>10</td>
-              <td>10</td>
-              <td>Action</td>
-              <td className='text-red-500'>Delete</td>
+              <td className="px-6 py-4 text-center">10</td>
+              <td className="px-6 py-4 text-center">10</td>
+              <td className="px-6 py-4 text-center">Action</td>
+              <td className="px-6 py-4 text-center text-red-500 cursor-pointer">
+                Delete
+              </td>
             </tr>
           </tbody>
         </table>
