37f932ff8f1edc945ec0c77144ca7f17bb2e0194
mrinal1224 <iammrinalkc@gmail.com>
2025-02-05 23:36:36 +0530

    redux intro

--- LLD-3 React/redux-app/vite-project/src/App.jsx
+++ LLD-3 React/redux-app/vite-project/src/App.jsx
@@ -1,22 +1,36 @@
 import { useState } from "react";
 import "./App.css";
+import { useSelector , useDispatch } from "react-redux";
+import { increment, decrement ,reset } from "./slices/counterSlice";
+
 
 function App() {
+
+  let count = useSelector((state)=> state.counter.value)
+
+  const dispatch = useDispatch()
+
+  console.log(count)
  
 
   function handleIncrement() {
-    
+     dispatch(increment())
   }
 
   function handleDecrement() {
-    
+    dispatch(decrement())
+  }
+
+  function resetCounter(){
+    dispatch(reset())
   }
 
   return (
     <>
       <button onClick={handleIncrement}>+</button>
-      <p>0</p>
+      <p>{count}</p>
       <button onClick={handleDecrement}>-</button>
+      <button onClick={resetCounter}>Reset</button>
     </>
   );
 }
--- LLD-3 React/redux-app/vite-project/src/slices/counterSlice.js
+++ LLD-3 React/redux-app/vite-project/src/slices/counterSlice.js
@@ -10,11 +10,15 @@ export const counterSlice = createSlice({
     initialState,
 
     reducers:{
+        // immer
         increment:(state)=>{
-           state.value+1
+           state.value +=1
         },
         decrement:(state)=>{
-            state.value-1
+            if(state.value>0){
+                state.value-=1
+            }
+         
         },
         reset:(state)=>{
             state.value = 0
