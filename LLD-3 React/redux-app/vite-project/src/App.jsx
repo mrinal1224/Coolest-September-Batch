355ed2abdb05f88e14d460a6c1c959e16265904a
mrinal1224 <iammrinalkc@gmail.com>
2025-02-05 23:37:56 +0530

    notes pushed

--- LLD-3 React/redux-app/vite-project/src/App.jsx
+++ LLD-3 React/redux-app/vite-project/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+
 import "./App.css";
 import { useSelector , useDispatch } from "react-redux";
 import { increment, decrement ,reset } from "./slices/counterSlice";
