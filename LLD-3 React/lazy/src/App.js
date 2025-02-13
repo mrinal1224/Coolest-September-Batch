6585dde2cce7172e52cf32095ee4df79de09704b
mrinal1224 <iammrinalkc@gmail.com>
2025-02-13 22:44:57 +0530

    push

--- LLD-3 React/lazy/src/App.js
+++ LLD-3 React/lazy/src/App.js
@@ -1,30 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-import Movies from './components/Movies';
-import Navbar from './components/Navbar';
-import { BrowserRouter, Routes , Route } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
 
-function App() {
+const App = () => {
+  const [HomePage, setHomePage] = useState(null);
+  const [AboutPage, setAboutPage] = useState(null);
+  const [ContactPage, setContactPage] = useState(null);
+
+  useEffect(() => {
+    // Preload HomePage component
+    import('./components/Home').then((module) => setHomePage(() => module.default));
+  }, []);
+
+  const loadHomePage = () => {
+    import('./components/Home.js').then((module) => setHomePage(() => module.default));
+  };
+
+  const loadAboutPage = () => {
+    import('./components/About.js').then((module) => setAboutPage(() => module.default));
+  };
+
+  const loadContactPage = () => {
+    import('./components/Contact.js').then((module) => setContactPage(() => module.default));
+  };
+
   return (
-    <div className="App">
-     {/* <Movies/> */}
-    <BrowserRouter>
-    <Navbar/>
-
-    <Routes>
-     <Route path='/home' element={<Home/>}/>
-     <Route path='/about' element={<About/>}/>
-     <Route path='/contact' element={<Contact/>}/>
-
-    </Routes>
-
-    </BrowserRouter>
-
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/" onClick={loadHomePage}>Home</Link>
+            </li>
+            <li>
+              <Link to="/about" onClick={loadAboutPage}>About</Link>
+            </li>
+            <li>
+              <Link to="/contact" onClick={loadContactPage}>Contact</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={HomePage ? <Home/> : <div>Loading...</div>} />
+          <Route path="/about" element={AboutPage ? <AboutPage /> : <div>Loading...</div>} />
+          <Route path="/contact" element={ContactPage ? <ContactPage/> : <div>Loading...</div>} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
-
+};
 export default App;
--- LLD-3 React/lazy/src/components/Navbar.js
+++ LLD-3 React/lazy/src/components/Navbar.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import {Link} from "react-router-dom"
 
 function Navbar() {
+ const [homePage , setHomePage]= useState()
+
+
+  const loadHomePage = ()=>{
+    import('./Home.js').then((module)=>{
+        setHomePage(module.default)
+    })
+  }
+
+
   return (
     <div style={{margin:'3rem', display:'flex', justifyContent:'center'}}>
-     <Link style={{margin:'2rem'}} to='/home'>Home</Link>
+     <Link onClick={loadHomePage} style={{margin:'2rem'}} to='/home'>Home</Link>
      <Link style={{margin:'2rem'}} to='/about'>About</Link>
      <Link style={{margin:'2rem'}} to='/contact'>contact</Link>
     </div>
