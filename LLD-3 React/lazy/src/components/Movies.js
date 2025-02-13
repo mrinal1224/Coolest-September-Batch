4a21e631b2429fcb9c58cdc1e3a4d5327b1e826a
mrinal1224 <iammrinalkc@gmail.com>
2025-02-13 21:56:30 +0530

    push

--- LLD-3 React/lazy/src/components/Movies.js
+++ LLD-3 React/lazy/src/components/Movies.js
@@ -1,11 +1,21 @@
-import React from 'react'
-import { movies } from './movieData'
+import React, { useState } from 'react'
+// import { movies } from './movieData'
 
 function Movies() {
+    const [moviesData , setMoviesData] = useState([])
+
+    function getData(){
+     import('./movieData.js').then(function(module){
+        setMoviesData(module.movies)
+     })
+       
+    }
+
   return (
     <div>
         <h1>I am the Movies Component</h1>
-        <div>{JSON.stringify(movies)}</div>
+        <button onClick={getData}>Get Movies</button>
+        <div>{JSON.stringify(moviesData)}</div>
     </div>
   )
 }
