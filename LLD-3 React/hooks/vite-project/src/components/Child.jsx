6c3c2c8ebdcc6269c3e1ee10e6c4f83c9f171957
mrinal1224 <iammrinalkc@gmail.com>
2025-02-14 22:47:09 +0530

    push

--- LLD-3 React/hooks/vite-project/src/components/Child.jsx
+++ LLD-3 React/hooks/vite-project/src/components/Child.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-function Child({buttonName}) {
+function Child({buttonName, sayHelloFn}) {
     console.log('Child Re-Rendered again')
   return (
     <div>
         <button>{buttonName}</button>
+        <h1>{sayHelloFn()}</h1>
     </div>
   )
 }
--- LLD-3 React/hooks/vite-project/src/components/MemoComponent.jsx
+++ LLD-3 React/hooks/vite-project/src/components/MemoComponent.jsx
@@ -1,14 +1,32 @@
-import React, { useState } from "react";
+import React, { useState , useCallback, useEffect} from "react";
 import Child from "./Child";
 
 function MemoComponent() {
   const [count, setCount] = useState(0);
+
+  // function sayHello(){
+  //   return "hello"
+  // }
+
+
+  let sayHello = useCallback(()=>{
+    return 'Hello'
+  }, [])
+  
+ 
+
+
+
+
+  // A
+  // B
+
   return (
     <div>
       <button onClick={() => setCount(count + 1)}>Increment</button>
       <h3>count:{count} </h3>
 
-      <Child buttonName='Click Me'/>
+      <Child buttonName='Click Me' sayHelloFn={sayHello}/>
     </div>
   );
 }
