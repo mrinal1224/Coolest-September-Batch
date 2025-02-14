79997fccf934b5836c6c7e9426ed47c09cef50a2
mrinal1224 <iammrinalkc@gmail.com>
2025-02-14 22:01:46 +0530

    push

--- LLD-3 React/hooks/vite-project/src/components/Memo.jsx
+++ LLD-3 React/hooks/vite-project/src/components/Memo.jsx
@@ -1,14 +1,18 @@
-import React, { use, useState } from "react";
+import React, { useMemo, useState } from "react";
 
 function Memo() {
   const [count, setCount] = useState(0);
   const [number, setNumber] = useState(0);
 
   function doubleValue(num) {
+    console.log(`Running for the num ${num}`);
+    for (let i = 0; i < 1000000000; i++) {}
     return num * 2;
   }
 
-  const doubledValue = doubleValue(number);
+    //   const doubledValue = doubleValue(number);
+
+  const doubledValue = useMemo(() => doubleValue(number), [number]);
 
   return (
     <div>
