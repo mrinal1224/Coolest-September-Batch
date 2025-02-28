1923a23f0af305cdd41328fedd7a6b538bcfd027
mrinal1224 <iammrinalkc@gmail.com>
2025-02-28 22:57:36 +0530

    push

--- LLD-4 (MERN)/Class-2 (Server Side Development)/server/server.js
+++ LLD-4 (MERN)/Class-2 (Server Side Development)/server/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json()); // built in middleware
+
 const courses = [
   { id: 1, name: "java" },
   { id: 2, name: "JavaScript" },
@@ -18,25 +20,31 @@ app.get("/courses", (req, res) => {
 // get a specific course
 
 app.get("/courses/:id", (req, res) => {
-   console.log(req.params)
+  console.log(req.params);
   let course = courses.find((course) => course.id === parseInt(req.params.id));
-  console.log(course)// undefined null , 0 - false
- 
+  console.log(course); // undefined null , 0 - false
 
-  if(!course){
-     res.status(404).send('Did not find the Course')
+  if (!course) {
+    res.status(404).send("Did not find the Course");
   }
 
-  res.send(course)
+  res.send(course);
 });
 
 // post method
 
 // create a course
 
+app.post("/courses", (req, res) => {
+  const course = {
+    id: req.body.id,
+    name: req.body.name,
+  };
 
+  courses.push(course);
 
-
+  res.send(courses);
+});
 
 app.listen(8005, () => {
   console.log(`Server Started on port 8005`);
